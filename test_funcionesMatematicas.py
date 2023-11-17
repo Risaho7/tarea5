ba36803fae52ab31cc670f24493cb2df1ac06c24
Risaho7 <rianlgl@gmail.com>
2023-11-17 11:27:43 +0100

    Añado comentarios en el código

--- test_funcionesMatematicas.py
+++ test_funcionesMatematicas.py
@@ -1,29 +1,31 @@
 import unittest
 from funcionesMatematicas import sumar, restar, multiplicar, dividir
 
+#clase que recoge las pruebas unitarias
 class testFunciones(unittest.TestCase):
 
-    def test_sumar(self):
+    def test_sumar(self):    # Pruebas para la función de suma
         self.assertEqual(sumar(4, 3), 7)
         self.assertEqual(sumar(-2, 1), -1)    
         self.assertEqual(sumar(-1, -5), -6) 
 
-    def test_restar(self):
+    def test_restar(self):   # Pruebas para la función de resta
         self.assertEqual(restar(5, 2), 3)
         self.assertEqual(restar(-1, 1), -2)    
         self.assertEqual(restar(-1, -3), 2) 
 
-    def test_multiplicar(self):
+    def test_multiplicar(self):  # Pruebas para la función de multiplicación
         self.assertEqual(multiplicar(4, 3), 12)
         self.assertEqual(multiplicar(-2, 2), -4)    
         self.assertEqual(multiplicar(-2, -4), 8) 
 
-    def test_dividir(self):
+    def test_dividir(self):  # Pruebas para la función de división
         self.assertEqual(dividir(8, 2), 4)
         self.assertEqual(dividir(12, 3), 4)    
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError): # Comprobación de la excepción generada al dividir por cero
             dividir(10, 0)
 
 
 if __name__ == '__main__':
-    unittest.main()             
+    unittest.main()     
+     
